7eba30bf3dd8d241ad6c18c89630194908f7905f
dysomni <jbrock3852@gmail.com>
2018-10-27 13:11:33 -0500

    setup the bones

---
+++ same_game/controller.py
@@ -0,0 +1,1 @@
+# This module will intake commands and call the appropriate functions
---
+++ same_game/gameState.py
@@ -0,0 +1,1 @@
+# This module will store information on each iteration of the game, and be able to return modifications of the state
