bcbbe142e1bdc2ef975f824e4f38a3f1dfcc8abb
dysomni <jbrock3852@gmail.com>
2018-10-29 21:41:32 -0500

    detailed comments on the State class

--- same_game/game_state.py
+++ same_game/game_state.py
@@ -15,11 +15,12 @@ class State:
         self.name = name  # String to represent state
         self.size = size  # Int between 1-15 for the length and width
         self.colors = colors  # Int between 1-5 picking the number of colors
-        self.data = data
-        if self.data is None:
+        self.data = data # 2D numpy array using int x, y, and value for color
+        if self.data is None: # if no initial array is passed in, it will generate it's own
             data = self.setup()
         self.data = data
-        self.memory = []
+        self.memory = [] # an array of coordinates (each coordinate is an array of two ints)
+                         # that stores the values that have been visited by the sections function
 
     # def __init__(self, name, size, colors, data):
     #     self.name = name  # String to represent state
@@ -27,6 +28,7 @@ class State:
     #     self.colors = colors  # Int between 0-4 picking the number of colors
     #     self.data = data
 
+    # randomly creates a 2D numpy array for a game state with the specified parameters
     def setup(self):
         matrix = np.empty([self.size,self.size])
         for row in range(self.size):
@@ -34,46 +36,54 @@ class State:
                 matrix[row][col] = random.randint(1,self.colors)
         return matrix
 
+    # returns the int color value of the specified x y values
     def getColor(self, x, y):
         if x < self.size:
             if y < self.size:
                 return self.data[x][y]
 
+    # creates a list of possible sections to select for removal
+    # returns a list of arrays of coordinates that have the same color value and are touching
+    # list layout: [ [[x,y],[x,y]], [[x,y],[x,y],[x,y]], ... ]
     def sections(self):
-        self.memory = []
-        moves = []
-        for row in range(self.size):
-            for col in range(self.size):
-                if self.data[row, col] != 0:
-                    if [row, col] not in self.memory:
-                        self.memory.append([row,col])
-                        move = self.recSections([row,col], self.memory, [])
-                        moves.append([[row,col]] + move)
-        return moves
+        self.memory = [] # makes sure the memory array is cleared
+        moves = [] # creates a list to add the arrays of coordinates to
+        for row in range(self.size): # for every row (x coordinate)
+            for col in range(self.size): # for every column (y coordinate)
+                if self.data[row, col] != 0: # if the current coordinate is not 0 (empty)
+                    if [row, col] not in self.memory: # and if the current coordinate is not in the memory
+                        self.memory.append([row,col]) # add the coordinate to memory
+                        move = self.recSections([row,col], []) # call recursive function to check coordinates around the current for matches
+                        moves.append([[row,col]] + move) # concatenates original coordinate and returned touching coordinates into list
+        return moves # returns the list of arrays of coordinates
 
-    def recSections(self, current, memory, move):
-        move1, move2, move3, move4 = [], [], [], []
-        if current[0]-1 >= 0:
+    # recursive function to determine the touching coordinates of the passed in variable
+    # parameters: current - coordinate: [x,y], move - list that temporarily stores each branches' touching coordinates before concatenation
+    # returns: list of coordinates [x,y] that touch the original passed-in coordinate
+    def recSections(self, current, move):
+        move1, move2, move3, move4 = [], [], [], [] # creates the placeholders for each branch test
+        if current[0]-1 >= 0: # test to make sure the coordinate to the left is valid
+            # if the color value of the current equals the color value to the left    and   the color value to the left is not in the memory
             if self.data[current[0]][current[1]] == self.data[current[0]-1][current[1]] and [current[0]-1,current[1]] not in self.memory:
-                self.memory.append([current[0]-1, current[1]])
-                move1 = self.recSections([current[0] - 1, current[1]], self.memory, move1)
-                move1.append([current[0]-1, current[1]])
-        if current[0]+1 < self.size:
+                self.memory.append([current[0]-1, current[1]]) # add the color value to the left into the memory
+                move1 = self.recSections([current[0] - 1, current[1]], move1) # call the recursive function on the new coordinate, and assign returned list
+                move1.append([current[0]-1, current[1]]) # adds the coordinate passed to the list as well
+        if current[0]+1 < self.size: # test to make sure the coordinate to the right is valid
             if self.data[current[0]][current[1]] == self.data[current[0]+1][current[1]] and [current[0]+1,current[1]] not in self.memory:
                 self.memory.append([current[0]+1, current[1]])
-                move2 = self.recSections([current[0]+1, current[1]], self.memory, move2)
+                move2 = self.recSections([current[0]+1, current[1]], move2)
                 move2.append([current[0]+1, current[1]])
-        if current[1]-1 >= 0:
+        if current[1]-1 >= 0: # test to make sure the coordinate to the top is valid
             if self.data[current[0]][current[1]] == self.data[current[0]][current[1]-1] and [current[0], current[1]-1] not in self.memory:
                 self.memory.append([current[0], current[1]-1])
-                move3 = self.recSections([current[0], current[1]-1], self.memory, move3)
+                move3 = self.recSections([current[0], current[1]-1], move3)
                 move3.append([current[0], current[1]-1])
-        if current[1]+1 < self.size:
+        if current[1]+1 < self.size: # test to make sure the coordinate to the bottom is valid
             if self.data[current[0]][current[1]] == self.data[current[0]][current[1]+1] and [current[0], current[1]+1] not in self.memory:
                 self.memory.append([current[0], current[1]+1])
-                move4 = self.recSections([current[0], current[1]+1], self.memory, move4)
+                move4 = self.recSections([current[0], current[1]+1], move4)
                 move4.append([current[0], current[1]+1])
-        return move1 + move2 + move3 + move4
+        return move1 + move2 + move3 + move4 # combine the different branches into one list
 
 
 #     def remove(self):
