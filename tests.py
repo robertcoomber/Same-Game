755f9daa7d68df6d24943ca8f91661f7c6bb0008
james-bertrand <james.bertrand89@gmail.com>
2018-10-29 20:41:13 -0500

    Tweaked game_state and tests

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from same_game import game_state as state
 
 testArray = state.State("testArray", 2, 2)
+print("The generated board:")
 print(testArray.data)
+print("Possible moves:")
 print(testArray.sections())
