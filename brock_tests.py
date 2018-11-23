66cbe9b3bb19dd81a7b1010a384641186ce39ec4
dysomni <jbrock3852@gmail.com>
2018-11-22 20:57:37 -0600

    three new functions to the game_state, and a modification of the controller class that handles generates until a complete puzzle is found. bertrand and brock also have seperate test classes now

---
+++ brock_tests.py
@@ -0,0 +1,34 @@
+from same_game import game_state as state
+from same_game import controller
+
+def test_game_state():
+    testArray = state.State("testArray", 10, 4)
+    print("The generated board:")
+    print(testArray.data)
+    print("All Sections:")
+    print(testArray.sections())
+    print("Valid Moves:")
+    print(testArray.moves())
+    print("The moves left and amount of blocks removed for every action:")
+    for i in range(20):
+        if testArray.isEmpty() == False and testArray.movesLeft():
+            print(testArray.moves())
+            print(testArray.count(testArray.moves()[0]))
+            testArray.remove(testArray.moves()[0])
+    print("Final:")
+    print(testArray.data)
+    print('Is the board empty?')
+    print(testArray.isEmpty(),'\n')
+
+def test_controller():
+    testBoards = [
+        state.State("Test1", 10, 4),
+        state.State("Test2", 7, 3),
+    ]
+
+    controller.agentOnlyMetrics(testBoards)
+
+
+# Call functions here to run specific tests
+test_game_state()
+test_controller()
--- same_game/game_state.py
+++ same_game/game_state.py
@@ -139,3 +139,41 @@ class State:
             return True
         else:
             return False
+
+    # will return the frequency of each color in the table, with each number correlating to the position
+    # in the array returned
+    def amounts(self):
+        l = []
+        for i in range(self.colors+1):
+            l.append(0)
+        for row in range(self.size):
+            for col in range(self.size):
+                l[int(self.data[row][col])] = l[int(self.data[row][col])] + 1
+        return l
+
+    # returns the number that has the highest frequency (excluding 0)
+    def highest(self):
+        l = self.amounts()
+        highest = 1
+        for i in range(len(l)-2):
+            if l[i + 1] < l[i + 2]:
+                highest = i+2
+        return highest
+
+    # will change the specified amount of tiles randomly to the color that is most frequent
+    def changeATile(self, amount):
+        highest = self.highest()
+        l = self.amounts()
+        #print(highest)
+        for i in range(amount):
+            go = True
+            while go:
+                x = random.randint(0,self.size-1)
+                y = random.randint(0,self.size-1)
+                if self.data[x,y] != highest and self.data[x,y] != 0 and l[int(self.data[x,y])] != 0:
+                    self.data[x,y] = highest
+                    go = False
+
+
+
+
